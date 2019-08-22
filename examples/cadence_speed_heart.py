4db62bc55557165051fbb79f52d82f1499ccccf1
Don Tredinnick <dtredinnick@tibco.com>
2019-08-22 08:20:27 -0700

    Rewrite of code
    Added several class definitions, added logging, added stubs for restart logic, added exception handling, added graceful shut-down, and removed superfluous code.

--- examples/cadence_speed_heart.py
+++ examples/cadence_speed_heart.py
@@ -1,24 +1,8 @@
-# ANT - Cadence, Speed Sensor AND Heart Rate Monitor - Example
+# ANT - Team TIBCO-SVB Sensor Connectivity Script
 #
-# Copyright (c) 2012, Gustav Tiger <gustav@tiger.name>
+# Copyright (c) 2019, TIBCO Software Inc
 #
-# Permission is hereby granted, free of charge, to any person obtaining a
-# copy of this software and associated documentation files (the "Software"),
-# to deal in the Software without restriction, including without limitation
-# the rights to use, copy, modify, merge, publish, distribute, sublicense,
-# and/or sell copies of the Software, and to permit persons to whom the
-# Software is furnished to do so, subject to the following conditions:
-#
-# The above copyright notice and this permission notice shall be included in
-# all copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
-# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
-# DEALINGS IN THE SOFTWARE.
+# 
 
 from __future__ import absolute_import, print_function
 
@@ -28,115 +12,225 @@ from ant.base.message import Message
 
 import binascii
 import paho.mqtt.client as paho
+import json
 import logging
 import struct
 import threading
+import time
 import sys
 
-NETWORK_KEY= [0xb9, 0xa5, 0x21, 0xfb, 0xbd, 0x72, 0xc3, 0x45]
-broker="192.168.118.35"
-port=1883
+# This function provides the logic for handling the restart command
+# that would be received from MQTT 
+def on_ext_message(client, userdata, message):
+    print("Message received: " + message.payload)
+    commandDict = json.loads(message.payload)
+    commandstr = commandDict["command"]
+    print("Command: " + commandstr)
 
+    if commandstr == "restart":
+        node.stop()
+        print ("node stopped")
+        channel = ChannelMaster()
+        print ("Channel defined")
+        monitor = Monitor()
+        print ("Monitor defined")
+        channel.initialize(monitor)
+        channel.openChannels()
+        node.start()
+    else:
+        print("Invalid Command Received")
+
+# This function validates the connection to the MQTT broker.  This 
+# seems to be the only way to really tell if the connection was
+# successful.
+def on_ext_connect(client, userdata, flags, rc):
+    if rc == 0:
+        print("Connected to broker")      
+        global Connected
+        Connected = True
+    else:
+        print("Connected failed")
+
+# Set up logging
+logger = logging.getLogger("ant.easy.csh")
+logger.setLevel(logging.DEBUG)
+fh = logging.FileHandler('csh.log', 'a', None, False)
+fh.setLevel(logging.DEBUG)
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+fh.setFormatter(formatter)
+logger.addHandler(fh)
+logging.basicConfig(level=logging.INFO)
+logger.info('start run')
+
+
+# This key is as deifned by the Ant Specification
+NETWORK_KEY= [0xb9, 0xa5, 0x21, 0xfb, 0xbd, 0x72, 0xc3, 0x45]
+
+# Create an instance of node.  This needs to be performed here in order to have node in scope for
+# the command processing functions that are to be developed.
+node = Node()
+
+# MQTT Connection information for local in-pi communciation
+localMQTTbroker="localhost"
+localMQTTport=1883
+
+# MQTT Connection information for external communication
+extMQTTBroker="localhost"
+extMQTTPort=1883
+
+# Monitor sets up call back functions to handle messages associated with each of the sensor types.
+# To add more follow the pattern for each of these functions.  Also, to have the function get called
+# there must be a corresponding section in the ChannelMaster initialize function.
+#
+# Each one of the on_data functions provides the callback function for processing the paticular type 
+# of sensor message.
 class Monitor():
     def __init__(self):
-        self.heartrate = "n/a";
-        self.cadence = "n/a";
-        self.speed = "n/a";
-        self.mqttclient = paho.Client("control1")
-        self.mqttclient.connect(broker,port)
+        self.localmqttclient = paho.Client("localClient")
+        self.localmqttclient.connect(localMQTTbroker,localMQTTport)
 
     def on_data_heartrate(self, data):
-        #self.heartrate = str(data[7])
-        #print ("heart rate ", data)
         publishstr = '{"deviceType":120,"message":"'+format(data[0],'02x')+format(data[1],'02x')+format(data[2],'02x')+format(data[3],'02x')+format(data[4],'02x')+format(data[5],'02x')+format(data[6],'02x')+format(data[7],'02x')+'"}'
         print(publishstr)
-        self.mqttclient.publish("org.teamtibco.bikesensor.rawdata",publishstr)
-        #self.display()
+        logger.info(publishstr)
+        self.localmqttclient.publish("org.teamtibco.bikesensor.rawdata",publishstr)
 
     def on_data_cadence(self, data):
-        #self.cadence = str(data[3]*256 + data[2])
-        #self.speed = str(data[7]*256 + data[6])
-        #self.cadence = str(data[7]*256 + data[6])
-        #print ("cadence ", data)
         publishstr = '{"deviceType":122,"message":"'+format(data[0],'02x')+format(data[1],'02x')+format(data[2],'02x')+format(data[3],'02x')+format(data[4],'02x')+format(data[5],'02x')+format(data[6],'02x')+format(data[7],'02x')+'"}'
         print(publishstr)
-        self.mqttclient.publish("org.teamtibco.bikesensor.rawdata",publishstr)
-        #self.display()
+        logger.info(publishstr)
+        self.localmqttclient.publish("org.teamtibco.bikesensor.rawdata",publishstr)
 
     def on_data_speed(self, data):
-        #self.cadence = str(data[3]*256 + data[2])
-        #self.speed = str(data[7]*256 + data[6])
-        #self.speed = str(data[7]*256 + data[6])
-        #print ("speed ", data)
         publishstr = '{"deviceType":123,"message":"'+format(data[0],'02x')+format(data[1],'02x')+format(data[2],'02x')+format(data[3],'02x')+format(data[4],'02x')+format(data[5],'02x')+format(data[6],'02x')+format(data[7],'02x')+'"}'
         print(publishstr)
-        self.mqttclient.publish("org.teamtibco.bikesensor.rawdata",publishstr)
-        #self.display()
+        logger.info(publishstr)
+        self.localmqttclient.publish("org.teamtibco.bikesensor.rawdata",publishstr)
     
     def on_data_power(self, data):
-        #print ("power ", data)
         publishstr = '{"deviceType":11,"message":"'+format(data[0],'02x')+format(data[1],'02x')+format(data[2],'02x')+format(data[3],'02x')+format(data[4],'02x')+format(data[5],'02x')+format(data[6],'02x')+format(data[7],'02x')+'"}'
         print(publishstr)
-        self.mqttclient.publish("org.teamtibco.bikesensor.rawdata",publishstr)
-        #self.display()
+        logger.info(publishstr)
+        self.localmqttclient.publish("org.teamtibco.bikesensor.rawdata",publishstr)
 
-    def display(self):
-        string = "Hearthrate: " + self.heartrate + " Pedal revolutions: " + self.cadence + " Wheel revolutions: " + self.speed
+# ChannelMaster provides the intialization for each of the monitor channels.  Currently heartrate, cadence, power, and speed are
+# supported.  Any valid ANT channel can be added by following the same pattern. It also provides the function that opens the 
+# channels.
+class ChannelMaster():
+    def __init__(self):
+        print("Initializing Channel Master")
 
-        sys.stdout.write(string)
-        sys.stdout.flush()
-        sys.stdout.write("\b" * len(string))
+    def initialize(self, Monitor):
+        try:
+           self.channel_heartrate = node.new_channel(Channel.Type.BIDIRECTIONAL_RECEIVE)
+           self.channel_heartrate.on_broadcast_data = Monitor.on_data_heartrate
+           self.channel_heartrate.on_burst_data = Monitor.on_data_heartrate
+           self.channel_heartrate.set_period(8070)
+           self.channel_heartrate.set_search_timeout(60)
+           self.channel_heartrate.set_rf_freq(57)
+           self.channel_heartrate.set_id(0, 120, 0)
+           print("Initialized Heartrate")
+        except Exception as ex:
+            print(ex)
+
+        self.channel_cadence = node.new_channel(Channel.Type.BIDIRECTIONAL_RECEIVE)
+        self.channel_cadence.on_broadcast_data = Monitor.on_data_cadence
+        self.channel_cadence.on_burst_data = Monitor.on_data_cadence
+        self.channel_cadence.set_period(8102)
+        self.channel_cadence.set_search_timeout(60)
+        self.channel_cadence.set_rf_freq(57)
+        self.channel_cadence.set_id(0, 122, 0)
+        print("Initialized Cadence")
+
+        self.channel_speed = node.new_channel(Channel.Type.BIDIRECTIONAL_RECEIVE)
+        self.channel_speed.on_broadcast_data = Monitor.on_data_speed
+        self.channel_speed.on_burst_data = Monitor.on_data_speed
+        self.channel_speed.set_period(8118)
+        self.channel_speed.set_search_timeout(60)
+        self.channel_speed.set_rf_freq(57)
+        self.channel_speed.set_id(0, 123, 0)
+        print("Initialized Speed")
+
+        self.channel_power = node.new_channel(Channel.Type.BIDIRECTIONAL_RECEIVE)
+        self.channel_power.on_broadcast_data = Monitor.on_data_power
+        self.channel_power.on_burst_data = Monitor.on_data_power
+        self.channel_power.set_period(8182)
+        self.channel_power.set_search_timeout(60)
+        self.channel_power.set_rf_freq(57)
+        self.channel_power.set_id(0, 11, 0)
+        print("Initialized Power")
+
+    def openChannels(self):
+        self.channel_heartrate.open()
+        self.channel_cadence.open()
+        self.channel_speed.open()
+        self.channel_power.open()
+
+# This class handles control messages coming into the script.  The goal is to have these do shutdown, restart, 
+# and potentially even change logging levels.  This is currently incomplete.  The callback functions are 
+# provided eariler in the code.
+class CommunicationHandler():
+    def __init__(self):
+        print("Initializing external communications")
+        self.externalmqttclient = paho.Client("externalClient")
+        self.externalmqttclient.on_connect = on_ext_connect
+        self.externalmqttclient.on_message = on_ext_message
+        self.externalmqttclient.username_pw_set("dtredinnick", password="Tibco.123")
+        self.externalmqttclient.connect(extMQTTBroker,extMQTTPort)
+        self.externalmqttclient.loop_start()
+        print("Connected to External MQTT Broker!")
+
+    def subscribe_ext_message(self, subjectName):
+        self.externalmqttclient.subscribe(subjectName)
+        print("Subscribed to " + subjectName)
 
 
 def main():
-    # logging.basicConfig()
-
+    # Instantiate an instance of the Monitor class.  The Monitor class owns the MQTT connection, and 
+    # provides the callback functions for the various sensor messages
     monitor = Monitor()
-
-    node = Node()
-    node.set_network_key(0x00, NETWORK_KEY)
-
-    channel = node.new_channel(Channel.Type.BIDIRECTIONAL_RECEIVE)
-    channel.on_broadcast_data = monitor.on_data_heartrate
-    channel.on_burst_data = monitor.on_data_heartrate
-    channel.set_period(8070)
-    channel.set_search_timeout(12)
-    channel.set_rf_freq(57)
-    channel.set_id(0, 120, 0)
-
-    channel_cadence = node.new_channel(Channel.Type.BIDIRECTIONAL_RECEIVE)
-    channel_cadence.on_broadcast_data = monitor.on_data_cadence
-    channel_cadence.on_burst_data = monitor.on_data_cadence
-    channel_cadence.set_period(8102)
-    channel_cadence.set_search_timeout(30)
-    channel_cadence.set_rf_freq(57)
-    channel_cadence.set_id(0, 122, 0)
-
-    channel_speed = node.new_channel(Channel.Type.BIDIRECTIONAL_RECEIVE)
-    channel_speed.on_broadcast_data = monitor.on_data_speed
-    channel_speed.on_burst_data = monitor.on_data_speed
-    channel_speed.set_period(8118)
-    channel_speed.set_search_timeout(30)
-    channel_speed.set_rf_freq(57)
-    channel_speed.set_id(0, 123, 0)
-
-    channel_power = node.new_channel(Channel.Type.BIDIRECTIONAL_RECEIVE)
-    channel_power.on_broadcast_data = monitor.on_data_power
-    channel_power.on_burst_data = monitor.on_data_power
-    channel_power.set_period(8182)
-    channel_power.set_search_timeout(30)
-    channel_power.set_rf_freq(57)
-    channel_power.set_id(0, 11, 0)
-
-    try:
-        channel.open()
-        channel_cadence.open()
-        channel_speed.open()
-        channel_power.open()
-        node.start()
-    finally:
-        node.stop()
     
+    # Set the network key for the node
+    node.set_network_key(0x00, NETWORK_KEY)
+    
+    # Create and initialze the channel.  This must be performed before the node can be started.
+    channel = ChannelMaster()
+    channel.initialize(monitor)
+    
+    # Create an instance of the communication handler and subscribe to the MQTT control messages.  
+    commHandler = CommunicationHandler()
+    commHandler.subscribe_ext_message("org.teamtibco.antdevice.control")
+
+    # Within the try block perform the following steps:
+    #  1) open the channels
+    #  2) start the node (This creates a listen loop, so the remaining steps won't get executed until the 
+    #     node is stopped.)
+    #  3) create an infinite loop in order to keep listening for MQTT control messages since at this point
+    #     we can no longer take advatage of the node loop.  There may be a more elegant way to do this, 
+    #     but for now this works.  This loop is really for future development of the control functions.
+    #
+    # There are two exception handlers.  The first one looks for a Ctrl-C and then initiates a graceful shutdown.
+    # The second one catches any other errors that may have occurred and then does a graceful shutdown.  
+    # 
+    # The finally code is what executes the shutdown.
+    try:
+        channel.openChannels()
+        print("Channels open!")
+        node.start()
+        while True:
+          time.sleep(0.5)
+
+        # print("Node Started!")
+    except KeyboardInterrupt:
+        print("Received Ctrl-C")
+    except:
+        print("Channel Failure")
+    finally:
+        print("Shutting Down Node")
+        logger.info("Shutting Down Node")
+        node.stop()
+
+# No idea what this last line does.      
 if __name__ == "__main__":
     main()
 
